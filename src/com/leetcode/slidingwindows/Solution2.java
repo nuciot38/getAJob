77f2ff941cfb3863c22693db196e4883ba8afa21
忆倾城 <584969846@qq>
2020-04-09 14:20:36 +0800

    commit sliding windows

---
+++ src/com/leetcode/slidingwindows/Solution2.java
@@ -0,0 +1,50 @@
+package com.leetcode.slidingwindows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LeetCode 3 无重复字符的最长子串
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 输入abcabcbb， 输出3
+ */
+
+
+public class Solution2 {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    /**
+     * 1.定义临时数组和最大数组
+     * 2.当临时数组包含当前字符时，比较临时数组的大小是否大于最大数组
+     * 3.如果大于，则将当前临时数组赋值给最大值数组
+     * 4.获取当前比较数组在临时数组中的下标，将下标及以前的元素remove出去
+     * 5.完成上述操作后，将当前元素add到临时数组中
+     * 6.比较临时数组和最大数组，返回最大值
+     */
+
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] ch = s.toCharArray();
+        List<String> tempList = new ArrayList<>();
+        List<String> maxList = new ArrayList<>();
+        for (int i = 0; i < ch.length; i++) {
+            String str = new String(new char[ch[i]]);
+            if (tempList.contains(str)) {
+                if (tempList.size() > maxList.size()) {
+                    maxList = new ArrayList<>(tempList);
+                }
+                int index = tempList.indexOf(str);
+                for (int j = 0; j <= index; j++) {
+                    tempList.remove(0);
+                }
+            }
+            tempList.add(str);
+        }
+        return Math.max(tempList.size(), maxList.size());
+    }
+}
---
+++ src/com/leetcode/slidingwindows/Solution3.java
@@ -0,0 +1,45 @@
+package com.leetcode.slidingwindows;
+
+/**
+ * LeetCode 424 替换后的最长重复字符
+ * 给你一个仅由大写英文字母组成的字符串，
+ * 你可以将任意位置上的字符替换成另外的字符，总共可最多替换 k 次。
+ * 在执行上述操作后，找到包含重复字母的最长子串的长度。
+ */
+public class Solution3 {
+    public static void main(String[] args) {
+        String s = "ABAB";
+        int k = 2;
+        System.out.println(characterReplacement(s, k));
+    }
+
+    /**
+     * 只考虑大写字母，可以定义一个整型数组，用于存放每个字母的个数
+     * 1.窗口的left和right初始为0,，开始循环后，right++
+     * 2.通过字节相减获取当前字母是第几个，从而对数组中当前元素++，表示当前窗口中该字母的个数
+     * 3.求出当前窗口中最多元素的个数
+     * 4.比较当前窗口大小是否大于当前窗口所能包含的最多相同字母数
+     * 5.如果大于，则将left的字母从窗口中移除出去，并且left++，此时窗口的大小同上一步循环大小相等
+     */
+
+    private static int[] map = new int[26];
+    public static int characterReplacement(String s, int k) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] ch = s.toCharArray();
+        int max = 0;
+        int left = 0;
+        int right = 0;
+        for (right = 0; right < ch.length; right++) {
+            int index = ch[right] - 'A';
+            map[index]++;
+            max = Math.max(max, map[index]);
+            if (right - left + 1 > max + k) {
+                map[ch[left] - 'A']--;
+                left++;
+            }
+        }
+        return right - left;
+    }
+}
